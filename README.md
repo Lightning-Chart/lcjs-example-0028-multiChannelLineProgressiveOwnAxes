f591dbc236c02a192189eddf06435b37bc81cfe4
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:47:04 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Multi-channel real-time data monitoring JavaScript Chart
 
-![Multi-channel real-time data monitoring JavaScript Chart](multiChannelLineProgressiveOwnAxes.png)
+![Multi-channel real-time data monitoring JavaScript Chart](multiChannelLineProgressiveOwnAxes-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -26,7 +26,7 @@ Widely used in all kinds of fields for monitoring live data from many (hundreds 
 Frames rendered per second (FPS) is recorded live, and displayed on the chart title. FPS of 40-60 indicates a smooth running performance.
 
 This line chart uses smooth anti-aliased thick lines for the best visual look, which is pretty GPU heavy with high data rates.
-For a lighter alternative you can check out [a similar line chart with best-performance graphics](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0010-multiChannelLineProgressive.html)
+For a lighter alternative you can check out [a similar line chart with best-performance graphics](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0010-multiChannelLineProgressive.html)
 
 ## Automatic Data Cleaning
 
@@ -36,7 +36,6 @@ Because LightningChart JS is designed for real-time data streaming solutions, it
 
 In practice, this results in an application where you can even scroll back for some distance and see older data, but if you scroll far enough, you will find that the old data has been cleaned. This allows the application to run _forever_!
 
-
 ## Several times proven performance leader in JS charting
 
 LightningChart is the pioneer group among JavaScript chart providers who started the study of data visualization performance comparison.
@@ -44,21 +43,21 @@ This activity is based on open-source applications that test several different c
 
 During the test, different metrics of performance are measured which indicate how efficiently the data visualization software performs. Common metrics are:
 
-- _Refresh rate_ (FPS), how rapidly the displayed visualization is updated as times per second.
-- _CPU usage_ (%), how much of available computing resource is utilized.
-    - This is very important, because almost entirety of the computing power allocated to browsing a single web page is SHARED between all the content on the web page.
-    - In simple terms, if you have a badly performing chart on your web page then it means your whole page will perform bad.
-- _Data amount_, how much data can be shown. In practice, limitations could be imposed on how long history can be displayed or how precisely the data can be visualized. 
+-   _Refresh rate_ (FPS), how rapidly the displayed visualization is updated as times per second.
+-   _CPU usage_ (%), how much of available computing resource is utilized.
+    -   This is very important, because almost entirety of the computing power allocated to browsing a single web page is SHARED between all the content on the web page.
+    -   In simple terms, if you have a badly performing chart on your web page then it means your whole page will perform bad.
+-   _Data amount_, how much data can be shown. In practice, limitations could be imposed on how long history can be displayed or how precisely the data can be visualized.
 
 **Since November 2021**, LightningChart JS has dominated all performance tests which have since covered a large set of data visualization use cases in various industries:
 
-- [_Static line graphs_](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0017-largeLineChartXY.html) | These days LightningChart loads far larger data sets and over 50 times faster than your average web chart. 
+-   [_Static line graphs_](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0017-largeLineChartXY.html) | These days LightningChart loads far larger data sets and over 50 times faster than your average web chart.
 
-- [_Real-time line charts_](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0010-multiChannelLineProgressive.html) | In the original motivation for its development, LightningChart JS performs over **500000** times more efficiently than the average JS chart - processing tens of thousands more data samples, using 4 times less processing power and refreshing 7 times in the same time others do just once.
+-   [_Real-time line charts_](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0010-multiChannelLineProgressive.html) | In the original motivation for its development, LightningChart JS performs over **500000** times more efficiently than the average JS chart - processing tens of thousands more data samples, using 4 times less processing power and refreshing 7 times in the same time others do just once.
 
-- [_Heatmap charts_](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0803-scrollingHeatmap.html) | Traditional JS heatmaps can be used to visualize small grids with total of 1000 data points and under. LCJS enables visualization of BILLION large data sets that could cover for example large geographical areas such as entire countries.
+-   [_Heatmap charts_](https:/lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0803-scrollingHeatmap.html) | Traditional JS heatmaps can be used to visualize small grids with total of 1000 data points and under. LCJS enables visualization of BILLION large data sets that could cover for example large geographical areas such as entire countries.
 
-**Find all the information of open-source LightningChart JS performance studies on our** [web page](https://www.arction.com/high-performance-javascript-charts/)!
+**Find all the information of open-source LightningChart JS performance studies on our** [web page](https://lightningchart.com/high-performance-javascript-charts/)!
 
 
 ## API Links
@@ -73,25 +72,25 @@ During the test, different metrics of performance are measured which indicate ho
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[XY cartesian chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html
-[Line series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lineseries.html
-[Data patterns]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/datapattern.html
-[Axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis.html
+[XY cartesian chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/ChartXY.html
+[Line series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LineSeries.html
+[Data patterns]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/interfaces/DataPattern.html
+[Axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis.html
 
--- src/index.js
+++ src/index.js
@@ -2,201 +2,214 @@
  * Lightning-fast Line Chart visualization over multiple channels that progress on the same X Axis
  */
 
-const lcjs = require("@arction/lcjs");
-const xydata = require('@arction/xydata')
+const lcjs = require('@arction/lcjs')
 
 // NOTE: Assuming predefined number of stacked channels.
-const SIGNALS = new Array(10).fill(0).map((_, i) => ({
-  title: `Ch ${i + 1}`,
-}));
-
-const DEFAULT_X_RANGE_MS = 30 * 1000;
+const SIGNALS = new Array(5).fill(0).map((_, i) => ({
+    title: `Ch ${i + 1}`,
+}))
+const DEFAULT_X_RANGE_MS = 30 * 1000
+const PADDING_BOTTOM = 30
+const PADDING_TOP = 40
+const PADDING_LEFT = 100
+const PADDING_AXIS_Y = 6
 
 const {
-  lightningChart,
-  AutoCursorModes,
-  emptyLine,
-  AxisTickStrategies,
-  AxisScrollStrategies,
-  synchronizeAxisIntervals,
-  UIOrigins,
-  UIDraggingModes,
-  translatePoint,
-  LegendBoxBuilders,
-  Themes,
-} = lcjs;
+    lightningChart,
+    AutoCursorModes,
+    emptyLine,
+    AxisTickStrategies,
+    AxisScrollStrategies,
+    synchronizeAxisIntervals,
+    UIOrigins,
+    UIDraggingModes,
+    LegendBoxBuilders,
+    Themes,
+} = lcjs
 
-const { createProgressiveFunctionGenerator } = xydata;
+const exampleContainer = document.getElementById('chart') || document.body
 
 const dashboard = lightningChart()
-  .Dashboard({
-    numberOfColumns: 1,
-    numberOfRows: SIGNALS.length,
-    // theme: Themes.darkGold
-  })
-  .setSplitterStyle(emptyLine);
+    .Dashboard({
+        numberOfColumns: 1,
+        numberOfRows: SIGNALS.length,
+        // theme: Themes.darkGold
+    })
+    .setSplitterStyle(emptyLine)
+
+/**
+ * Function updates heights of charts in Dashboard. Should be called on resize/etc. for interactive applications.
+ */
+const layoutDashboard = () => {
+    const totalHeight = exampleContainer.getBoundingClientRect().height
+    const signalHeight = (totalHeight - PADDING_BOTTOM - PADDING_TOP) / SIGNALS.length
+
+    SIGNALS.forEach((_, iSignal) => {
+        const chHeight = signalHeight + (iSignal === 0 ? PADDING_TOP : 0) + (iSignal === SIGNALS.length - 1 ? PADDING_BOTTOM : 0)
+        dashboard.setRowHeight(iSignal, chHeight)
+    })
+}
+requestAnimationFrame(layoutDashboard)
 
 const channels = SIGNALS.map((signal, iSignal) => {
-  const chart = dashboard
-    .createChartXY({
-      columnIndex: 0,
-      rowIndex: iSignal,
-    })
-    .setTitle("")
-    .setPadding({ top: 0, bottom: 0 })
-    .setAutoCursorMode(AutoCursorModes.disabled)
-    .setBackgroundStrokeStyle(emptyLine)
-    .setMouseInteractions(false);
+    const chart = dashboard
+        .createChartXY({
+            columnIndex: 0,
+            rowIndex: iSignal,
+        })
+        .setTitle('')
+        .setPadding({
+            top: iSignal > 0 ? PADDING_AXIS_Y : 0,
+            bottom: iSignal < SIGNALS.length - 1 ? PADDING_AXIS_Y : 0,
+            left: 0,
+        })
+        .setAutoCursorMode(AutoCursorModes.disabled)
+        .setBackgroundStrokeStyle(emptyLine)
+        .setMouseInteractions(false)
 
-  const axisX = chart
-    .getDefaultAxisX()
-    .setTickStrategy(AxisTickStrategies.Empty)
-    .setStrokeStyle(emptyLine)
-    .setScrollStrategy(AxisScrollStrategies.progressive)
-    .setInterval(0, 10, false, true)
-  const axisY = chart
-    .getDefaultAxisY()
-    .setTickStrategy(AxisTickStrategies.Empty)
-    .setStrokeStyle(emptyLine)
-    .setTitle(signal.title)
-    .setTitleRotation(0)
-    .setThickness(60);
+    const axisX = chart
+        .getDefaultAxisX()
+        .setTickStrategy(AxisTickStrategies.Empty)
+        .setStrokeStyle(emptyLine)
+        .setScrollStrategy(AxisScrollStrategies.progressive)
+        .setInterval({ start: -DEFAULT_X_RANGE_MS, end: 0, stopAxisAfter: false })
+    const axisY = chart
+        .getDefaultAxisY()
+        .setTickStrategy(AxisTickStrategies.Empty)
+        .setStrokeStyle(emptyLine)
+        .setTitle(signal.title)
+        .setTitleRotation(0)
+        .setThickness(60)
 
-  const series = chart
-    .addLineSeries({
-      dataPattern: { pattern: "ProgressiveX" },
-      automaticColorIndex: iSignal,
-    })
-    .setName(`Channel ${iSignal + 1}`)
-    .setDataCleaning({ minDataPointCount: 10000 });
+    const series = chart
+        .addLineSeries({
+            dataPattern: { pattern: 'ProgressiveX' },
+            automaticColorIndex: iSignal,
+        })
+        .setName(`Channel ${iSignal + 1}`)
+        .setDataCleaning({ minDataPointCount: 10000 })
+        // Use 2 thickness for smooth anti-aliased thick lines with the best visual look, this is pretty GPU heavy.
+        .setStrokeStyle((style) => style.setThickness(2))
 
-  return { chart, series, axisX, axisY };
-});
-const channelTop = channels[0];
-const channelBottom = channels[channels.length - 1];
+    return { chart, series, axisX, axisY }
+})
+const channelTop = channels[0]
+const channelBottom = channels[channels.length - 1]
 
-channelTop.chart
-  .setTitle("Multi-channel real-time monitoring (10 chs, 1000 Hz)")
-  .setPadding({ top: 8 });
+channelTop.chart.setTitle(`Multi-channel real-time monitoring (${SIGNALS.length} chs, 1000 Hz)`)
 
 const axisX = channelBottom.axisX
-  .setTickStrategy(AxisTickStrategies.Time, (ticks) =>
-    ticks
-      .setMajorTickStyle((major) => major.setGridStrokeStyle(emptyLine))
-      .setMinorTickStyle((minor) => minor.setGridStrokeStyle(emptyLine))
-  )
-synchronizeAxisIntervals(
-  axisX,
-  ...channels.map((ch) => ch.axisX).filter((axis) => axis !== axisX)
-);
-axisX.setInterval(-DEFAULT_X_RANGE_MS, 0);
+    .setThickness(PADDING_BOTTOM)
+    .setTickStrategy(AxisTickStrategies.Time, (ticks) =>
+        ticks
+            .setMajorTickStyle((major) => major.setGridStrokeStyle(emptyLine))
+            .setMinorTickStyle((minor) => minor.setGridStrokeStyle(emptyLine)),
+    )
+synchronizeAxisIntervals(...channels.map((ch) => ch.axisX))
+
+// axisX.setInterval({ start: -DEFAULT_X_RANGE_MS, end: 0, stopAxisAfter: false })
 
 // Add legend
-const legend = dashboard.addLegendBox(LegendBoxBuilders.VerticalLegendBox, dashboard.uiScale)
-  .setPosition({ y: 50, x: translatePoint({ x: channelBottom.chart.pixelScale.x.getCellSize() - channelBottom.chart.getPadding().right, y: 0 }, channelBottom.chart.pixelScale, dashboard.uiScale).x })
-  .setOrigin(UIOrigins.RightCenter)
-channels.forEach(channel => legend.add(channel.series))
+const legend = dashboard
+    .addLegendBox(LegendBoxBuilders.HorizontalLegendBox)
+    .setPosition({ x: 50, y: 0 })
+    .setOrigin(UIOrigins.CenterBottom)
+    .setMargin({ bottom: PADDING_BOTTOM })
+    .setDraggingMode(UIDraggingModes.notDraggable)
+channels.forEach((channel) => legend.add(channel.series))
 
 // Custom interactions for zooming in/out along Time axis while keeping data scrolling.
-axisX
-  .setNibInteractionScaleByDragging(false)
-  .setNibInteractionScaleByWheeling(false)
-  .setAxisInteractionZoomByWheeling(false);
+axisX.setNibInteractionScaleByDragging(false).setNibInteractionScaleByWheeling(false).setAxisInteractionZoomByWheeling(false)
 const customZoomX = (_, event) => {
-  const interval = axisX.getInterval();
-  const range = interval.end - interval.start;
-  const newRange = range + Math.sign(event.deltaY) * 0.1 * Math.abs(range);
-  axisX.setInterval(interval.end - newRange, interval.end, false, false);
-  event.preventDefault()
-  event.stopPropagation()
-};
-axisX.onAxisInteractionAreaMouseWheel(customZoomX);
-channels.forEach((channel) =>
-  channel.chart.onSeriesBackgroundMouseWheel(customZoomX)
-);
+    const interval = axisX.getInterval()
+    const range = interval.end - interval.start
+    const newRange = range + Math.sign(event.deltaY) * 0.1 * Math.abs(range)
+    axisX.setInterval({ start: interval.end - newRange, end: interval.end, stopAxisAfter: false })
+    event.preventDefault()
+    event.stopPropagation()
+}
+axisX.onAxisInteractionAreaMouseWheel(customZoomX)
+channels.forEach((channel) => {
+    channel.chart.onSeriesBackgroundMouseWheel(customZoomX)
+    channel.series.onMouseWheel(customZoomX)
+})
 
 // Add LCJS user interface button for resetting view.
 const buttonReset = dashboard
-  .addUIElement()
-  .setText("Reset")
-  .setPosition({ x: 0, y: 0 })
-  .setOrigin(UIOrigins.LeftBottom)
-  .setMargin({ left: 4, bottom: 4 })
-  .setDraggingMode(UIDraggingModes.notDraggable);
+    .addUIElement()
+    .setText('Reset')
+    .setPosition({ x: 0, y: 0 })
+    .setOrigin(UIOrigins.LeftBottom)
+    .setMargin({ left: 4, bottom: 4 })
+    .setDraggingMode(UIDraggingModes.notDraggable)
 buttonReset.onMouseClick((_) => {
-  const xMax = channels[0].series.getXMax();
-  axisX.setInterval(xMax - DEFAULT_X_RANGE_MS, xMax, false, false);
-  channels.forEach((channel) => channel.axisY.fit());
-});
-
-// Define unique signals that will be used for channels.
-const signals = [
-  { length: 400 * Math.PI, func: (x) => Math.sin(x / (200)) },
-  { length: 400 * Math.PI, func: (x) => Math.cos(x / (200)) },
-  { length: 800 * Math.PI, func: (x) => Math.cos(x / (400)) + Math.sin(x / (200)) },
-  { length: 800 * Math.PI, func: (x) => Math.sin(x / (100)) +  Math.cos(x / (400)) },
-  { length: 800 * Math.PI, func: (x) => Math.sin(x / (200)) * Math.cos(x / (400)) },
-  { length: 1800 * Math.PI, func: (x) => Math.cos(x / (900)) },
-  { length: 3200 * Math.PI, func: (x) => Math.sin(x / (1600)) },
-  { length: 2600 * Math.PI, func: (x) => Math.sin(x / (400)) * Math.cos(x / (1300)) },
-]
-// Generate data sets for each signal.
-Promise.all(
-  signals.map((signal) =>
-      createProgressiveFunctionGenerator()
-          .setStart(0)
-          .setEnd(signal.length)
-          .setStep(1)
-          .setSamplingFunction(signal.func)
-          .generate()
-          .toPromise()
-          .then((data) => data.map((xy) => xy.y)),
-  ),
-)
-.then((dataSets) => {
-  // Stream data into series.
-  let xPrev = 0
-  let iSample = 0
-  const streamData = () => {
-    const newDataPointsCount = 60; // Matches 1000 Hz very roughly
-    const xNow = window.performance.now()
-    const xDelta = xNow - xPrev
-    const seriesNewDataPoints = []
-    for (let iChannel = 0; iChannel < channels.length; iChannel++) {
-      const dataSet = dataSets[iChannel % (dataSets.length - 1)]
-      const newDataPoints = []
-      for (let iDp = 0; iDp < newDataPointsCount; iDp++) {
-          const x = xPrev + ((iDp + 1) / newDataPointsCount) * xDelta
-          const iData = (iSample + iDp) % (dataSet.length - 1)
-          const y = dataSet[iData]
-          const point = { x, y }
-          newDataPoints.push(point)
-      }
-      seriesNewDataPoints[iChannel] = newDataPoints
-    }
-    channels.forEach((channel, iChannel) => channel.series.add(seriesNewDataPoints[iChannel]))
-    xPrev = xNow
-    iSample += newDataPointsCount
-    requestAnimationFrame(streamData)
-  }
-  streamData()
+    const xMax = channels[0].series.getXMax()
+    axisX.setInterval({ start: xMax - DEFAULT_X_RANGE_MS, end: xMax, stopAxisAfter: false })
+    channels.forEach((channel) => channel.axisY.fit())
 })
 
-// Measure FPS.
-let tFpsStart = window.performance.now();
-let frames = 0;
-let fps = 0;
-const title = channelTop.chart.getTitle();
-const recordFrame = () => {
-  frames++;
-  const tNow = window.performance.now();
-  fps = 1000 / ((tNow - tFpsStart) / frames);
-  requestAnimationFrame(recordFrame);
+// Generate data sets that is repeated for each channel for demonstration purposes.
+const dataSets = [
+    { length: Math.ceil(400 * Math.PI), func: (x) => 8 * Math.sin(x / 200) },
+    { length: Math.ceil(3200 * Math.PI), func: (x) => 7 * Math.sin(x / 1600) },
+    { length: Math.ceil(800 * Math.PI), func: (x) => 4 * (Math.cos(x / 400) + Math.sin(x / 200)) },
+    { length: Math.ceil(800 * Math.PI), func: (x) => 6 * Math.sin(x / 100) + Math.cos(x / 400) },
+    { length: Math.ceil(1800 * Math.PI), func: (x) => 8 * Math.cos(x / 900) },
+].map((config) => {
+    const data = []
+    data.length = config.length
+    for (let i = 0; i < config.length; i += 1) {
+        const y = config.func(i)
+        data[i] = y
+    }
+    return data
+})
 
-  channelTop.chart.setTitle(`${title} (FPS: ${fps.toFixed(1)})`);
-};
-requestAnimationFrame(recordFrame);
+// Stream data into series.
+let tStart = window.performance.now()
+let pushedDataCount = 0
+const dataPointsPerSecond = 1000 // 1000 Hz
+const xStep = 1000 / dataPointsPerSecond
+const streamData = () => {
+    const tNow = window.performance.now()
+    // NOTE: This code is for example purposes (streaming stable data rate without destroying browser when switching tabs etc.)
+    // In real use cases, data should be pushed in when it comes.
+    const shouldBeDataPointsCount = Math.floor((dataPointsPerSecond * (tNow - tStart)) / 1000)
+    const newDataPointsCount = Math.min(shouldBeDataPointsCount - pushedDataCount, 1000) // Add max 1000 data points per frame into a series. This prevents massive performance spikes when switching tabs for long times
+    const seriesNewDataPoints = []
+    for (let iChannel = 0; iChannel < channels.length; iChannel++) {
+        const dataSet = dataSets[iChannel % dataSets.length]
+        const newDataPoints = []
+        for (let iDp = 0; iDp < newDataPointsCount; iDp++) {
+            const x = (pushedDataCount + iDp) * xStep
+            const iData = (pushedDataCount + iDp) % dataSet.length
+            const y = dataSet[iData]
+            const point = { x, y }
+            newDataPoints.push(point)
+        }
+        seriesNewDataPoints[iChannel] = newDataPoints
+    }
+    channels.forEach((channel, iChannel) => channel.series.add(seriesNewDataPoints[iChannel]))
+    pushedDataCount += newDataPointsCount
+    requestAnimationFrame(streamData)
+}
+streamData()
+
+// Measure FPS.
+let tFpsStart = window.performance.now()
+let frames = 0
+let fps = 0
+const title = channelTop.chart.getTitle()
+const recordFrame = () => {
+    frames++
+    const tNow = window.performance.now()
+    fps = 1000 / ((tNow - tFpsStart) / frames)
+    requestAnimationFrame(recordFrame)
+
+    channelTop.chart.setTitle(`${title} (FPS: ${fps.toFixed(1)})`)
+}
+requestAnimationFrame(recordFrame)
 setInterval(() => {
-  tFpsStart = window.performance.now();
-  frames = 0;
-}, 5000);
+    tFpsStart = window.performance.now()
+    frames = 0
+}, 5000)
